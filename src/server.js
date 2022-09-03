60245c0c644ce331fc0fb25dcf128d53b52688f3
hoizzy <dbrtyu@naver.com>
2022-09-03 22:44:18 +0900

    add middleware used app.use function

--- src/server.js
+++ src/server.js
@@ -4,15 +4,30 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected"){
+        return res.send("Not Allowed");
+    };
+    console.log("Allowed, you may continue.");
+    next();
+};
+
 const handleHome = (req, res) => {
     return res.end();
-}
-const handleLogin = (req, res) => {
-    return res.send("Login page");
-}
+};
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge");
+};
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
