ea06524d73a48a474b7a3cb54f7dd0cae0495fa0
hoizzy <dbrtyu@naver.com>
2022-09-05 21:39:53 +0900

    make router named global, user, video

---
+++ README.md
@@ -0,0 +1,16 @@
+#Wetube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+
+/users/edit -> Edit User
+/users/delete -> Delete User
+
+/video/watch -> Watch Video
+/video/edit -> Edit Video
+/video/delete -> Delete Video
+/video/comments -> Comment on a Video
+/video/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -5,28 +5,35 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("Not Allowed");
-    };
-    console.log("Allowed, you may continue.");
-    next();
-};
-
-const handleHome = (req, res) => {
-    return res.end();
-};
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge");
-};
-
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () => {
+    console.log(`Server listening on port http://localhost:${PORT}`);
+}
 
 app.listen(PORT, handleListening);
