34286a99af4b4dee2abe048bc5a74bbfc9da8826
hoizzy <dbrtyu@naver.com>
2022-09-08 18:19:36 +0900

    make mixin named video.pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,33 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 2,
+        },
+        {
+            title: "Third",
+            rating: 5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 3,
+        },
+    ];
+    
+    res.render("home", {pageTitle: "Home", videos});
+}
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const remove = (req, res) => res.send("Remove");
