c1f3fc212b8c90ffb4a637fd10e8f7ed2301431d
hoizzy <dbrtyu@naver.com>
2022-09-09 13:31:16 +0900

    text watch page with fake database named videos

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,38 @@
+let videos = [
+    {
+        title: "First",
+        rating: 5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second",
+        rating: 5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third",
+        rating: 5,
+        comments:2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First",
-            rating: 5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second",
-            rating: 5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 2,
-        },
-        {
-            title: "Third",
-            rating: 5,
-            comments:2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 3,
-        },
-    ];
-    
     res.render("home", {pageTitle: "Home", videos});
 }
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
+export const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    res.render("watch", {pageTitle: `Watching ${video.title}`, videos});
+}
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const remove = (req, res) => res.send("Remove");
 export const search = (req, res) => res.send("Search");
