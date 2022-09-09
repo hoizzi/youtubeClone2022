0b8b3e820cc48ecfc122adf123e7b43b84717b2d
hoizzy <dbrtyu@naver.com>
2022-09-09 16:10:34 +0900

    make post router for editing video title on fake DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
         rating: 5,
         comments:2,
         createdAt: "2 minutes ago",
-        views: 59,
+        views: 1,
         id: 1,
     },
     {
@@ -27,13 +27,20 @@ let videos = [
 
 export const trending = (req, res) => {
     res.render("home", {pageTitle: "Home", videos});
-}
-export const see = (req, res) => {
+};
+export const watch = (req, res) => {
     const { id } = req.params;
     const video = videos[id-1];
-    res.render("watch", {pageTitle: `Watching ${video.title}`, videos});
-}
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
-export const remove = (req, res) => res.send("Remove");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
+    res.render("watch", {pageTitle: `Watching: ${video.title}`, video});
+};
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id-1];
+    res.render("edit", {pageTitle: `Editing: ${video.title}`, video});
+};
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    res.redirect(`/videos/${id}`);
+};
