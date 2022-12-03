5582c30028d787b227836a1be0c458f26404bd02
hoizzy <dbrtyu@naver.com>
2022-12-03 18:27:29 +0900

    save logged in status by using res.locals on middlewares.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -59,7 +59,8 @@ export const postLogin = async (req, res) => {
             errorMessage: "Wrong password."
         });
     }
-    //Logined user
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
 }
 
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -22,6 +23,7 @@ app.use(
     })
 );
 
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter); 
 app.use("/users", userRouter);
